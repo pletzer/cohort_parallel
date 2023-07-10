8eb5a01a0e948c480abed1bb3339a02ccaa67ba2
Alex Pletzer <alexander@gokliya.net>
2023-07-10 15:26:17 +1200

    extended tests

--- cohort_parallel/task.py
+++ cohort_parallel/task.py
@@ -2,35 +2,34 @@
 
 class Task:
 
-	def __init__(self, na, nt):
-		"""
-		Constructor
-		:param na: number of age groups
-		:param nt: number of time steps
-		"""
-		self.na = na
-		self.nt = nt
+    def __init__(self, na, nt):
+        """
+        Constructor
+        :param na: number of age groups
+        :param nt: number of time steps
+        """
+        self.na = na
+        self.nt = nt
 
-	def get_num_tasks(self):
-		return self.nt + self.na - 1
+    def get_num_tasks(self):
+        return self.nt + self.na - 1
 
-	def get_num_time_steps(self, task_id):
-		# if task_id < self.na
-		res = self.na - task_id
-		if self.na <= task_id < self.nt:
-			res = self.na
-		elif task_id >= self.nt:
-			res = self.na + self.nt - task_id - 1
-		return res
+    def get_num_time_steps(self, task_id):
+        # if task_id < self.na
+        res = self.na - task_id
+        if self.na <= task_id < self.nt:
+            res = self.na
+        elif task_id >= self.nt:
+            res = self.na + self.nt - task_id - 1
+        return res
 
-	def get_initial_dependencies(self, task_id):
-		res = None
-		if task_id >= 2*self.na - 1:
-			res = {i - 1 for i in range(task_id, self.na - 1)}
-		elif self.na <= task_id < 2*self.na - 1:
-			res = {i for i in range(task_id - self.na + 2)}
-			res += {i for i in range(self.na, task_id)}
-		return res
+    def get_initial_dependencies(self, task_id):
+        res = None
+        if task_id > 2*self.na - 1:
+            res = {i for i in range(task_id - self.na + 1, task_id)}
+        elif self.na <= task_id <= 2*self.na - 1:
+            res = {i for i in range(self.na + 3 - task_id)}.union({i for i in range(self.na, task_id)})
+        return res
 
 
 
--- cohort_parallel/tests/test_task.py
+++ cohort_parallel/tests/test_task.py
@@ -1,16 +1,33 @@
 from cohort_parallel import Task
 
 def test_task4_5():
-	t = Task(4, 8)
-	assert(t.get_num_tasks() == 11)
-	assert(t.get_num_time_steps(0) == 4)
-	assert(t.get_num_time_steps(1) == 3)
-	assert(t.get_num_time_steps(2) == 2)
-	assert(t.get_num_time_steps(3) == 1)
-	assert(t.get_num_time_steps(4) == 4)
-	assert(t.get_num_time_steps(5) == 4)
-	assert(t.get_num_time_steps(6) == 4)
-	assert(t.get_num_time_steps(7) == 4)
-	assert(t.get_num_time_steps(8) == 3)
-	assert(t.get_num_time_steps(9) == 2)
-	assert(t.get_num_time_steps(10) == 1)
+    t = Task(4, 8)
+
+    assert(t.get_num_tasks() == 11)
+    assert(t.get_num_time_steps(0) == 4)
+    assert(t.get_num_time_steps(1) == 3)
+    assert(t.get_num_time_steps(2) == 2)
+    assert(t.get_num_time_steps(3) == 1)
+    assert(t.get_num_time_steps(4) == 4)
+    assert(t.get_num_time_steps(5) == 4)
+    assert(t.get_num_time_steps(6) == 4)
+    assert(t.get_num_time_steps(7) == 4)
+    assert(t.get_num_time_steps(8) == 3)
+    assert(t.get_num_time_steps(9) == 2)
+    assert(t.get_num_time_steps(10) == 1)
+
+    assert(t.get_initial_dependencies(0) is None)
+    assert(t.get_initial_dependencies(1) is None)
+    assert(t.get_initial_dependencies(2) is None)
+    assert(t.get_initial_dependencies(3) is None)
+    assert(t.get_initial_dependencies(4) == {0, 1, 2})
+    assert(t.get_initial_dependencies(5) == {4, 0, 1})
+    assert(t.get_initial_dependencies(6) == {5, 4, 0})
+    assert(t.get_initial_dependencies(7) == {6, 5, 4})
+    assert(t.get_initial_dependencies(8) == {7, 6, 5})
+    assert(t.get_initial_dependencies(9) == {8, 7, 6})
+    assert(t.get_initial_dependencies(10) == {9, 8, 7})
+    
+
+if __name__ == '__main__':
+    test_task4_5()
