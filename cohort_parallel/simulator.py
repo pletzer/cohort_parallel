bb600e35423102de8d15d909cd19bb88b53e0f08
Alex Pletzer <alexander@gokliya.net>
2023-07-17 17:02:11 +1200

    added some communication between workers

--- cohort_parallel/simulator.py
+++ cohort_parallel/simulator.py
@@ -4,30 +4,34 @@ from worker import Worker
 
 
 def main(*, nt: int):
-	"""
-	Run a simulation
+    """
+    Run a simulation
 
-	:param nt: number of time steps
-	"""
-	comm = MPI.COMM_WORLD
-	worker_id = comm.Get_rank()
-	num_workers = comm.Get_size()
-	na = num_workers
+    :param nt: number of time steps
+    """
+    comm = MPI.COMM_WORLD
+    worker_id = comm.Get_rank()
+    num_workers = comm.Get_size()
+    na = num_workers
 
-	worker = Worker(na, nt, worker_id)
+    worker = Worker(na, nt, worker_id)
+    
+    list_of_executed_tasks = []
+    for it in range(nt):
+        tid = worker.get_task_to_execute()
+        step = worker.get_step()
 
-	list_of_executed_tasks = []
-	next_task = -1
-	while next_task is not None:
-		list_of_executed_tasks.append( (worker.get_task_to_execute(), worker.get_num_time_steps_to_execute()) )
-		next_task = worker.execute_task()
+        worker.execute_step()
 
-	fmt = ''
-	for t, m in list_of_executed_tasks:
-		fmt += f'{t}x{m} '
-	print(f'worker {worker_id} executed tasks {fmt}')
+        list_of_executed_tasks.append( (tid, step) )
+
+
+    fmt = ''
+    for t, m in list_of_executed_tasks:
+        fmt += f'{t}x{m} '
+    print(f'worker {worker_id} executed tasks {fmt}')
 
 if __name__ == '__main__':
-	defopt.run(main)
+    defopt.run(main)
 
-	
+    
