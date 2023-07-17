6a85bc890b8764ea7cca1d5be3c163dceafe0fb9
Alex Pletzer <alexander@gokliya.net>
2023-07-17 20:24:09 +1200

    fixed the logger

--- cohort_parallel/simulator.py
+++ cohort_parallel/simulator.py
@@ -1,10 +1,10 @@
 from mpi4py import MPI
 import defopt
 from worker import Worker
-from functools import reduce
-import logging
-logging.basicConfig(format='%(astime)s: %(message)%')
 
+import sys
+import logging
+logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.DEBUG)
 
 def main(*, nt: int):
     """
@@ -18,31 +18,44 @@ def main(*, nt: int):
     na = num_workers
 
     worker = Worker(na, nt, worker_id)
-    
-    # iterate over the time steps, gathering info about the time step, the worker id and the task id
+
+    # gather info about tasks are being executed
     list_of_executed_tasks = {}
+
+    #
+    # iterate over the time steps
+    # 
     for step in range(nt):
         tid = worker.get_task_to_execute()
         list_of_executed_tasks[step] = list_of_executed_tasks.get(step, {})
+
+        # execute the task
         worker.execute_step()
+
         list_of_executed_tasks[step][worker_id] = tid
+
+    #
+    # print the info
+    #
 
     # send the info to root
     lets = comm.gather(list_of_executed_tasks, root=0)
 
     if worker_id == 0:
-
-        # print the info
-
         # flatten the list of dicts into a single multilevel dict
         all_lets = {}
         for i in range(na):
             for step, item in lets[i].items():
                 all_lets[step] = all_lets.get(step, {}) | item
 
-        for it in range(nt):
+        print('step     ', end='')
+        for wid in range(na):
+            print(f'{wid:4d} ', end='')
+        print('\n' + '-'*(9 + (4 + 1)*na))
+        for step in range(nt):
+            print(f'{step:8d} ', end='')
             for wid in range(na):
-                print(f'{all_lets[it][wid]:4d} ', end='')
+                print(f'{all_lets[step][wid]:4d} ', end='')
             print()
 
 if __name__ == '__main__':
